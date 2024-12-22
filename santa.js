48609dbc69c2546aa5e7075bc2db79d3e57fe520
justinfinn2891 <jfinn5@ut.utm.edu>
2024-12-22 13:39:17 -0600

    new stuff

--- santa.js
+++ santa.js
@@ -1,7 +1,9 @@
 const button = document.querySelector("#christmasbutton");
 const tree = document.querySelector("#tree");
 const page = document.querySelector("#page");
+const background = document.querySelector("#background");
 button.addEventListener("click", () => {
+    background.classList.toggle("change");
     tree.classList.toggle("move-right");
     page.classList.toggle("build");
     button.disabled = true;
