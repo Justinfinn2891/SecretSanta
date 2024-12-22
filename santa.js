3c36a80801467d10f6164ea6d39004741be11c97
justinfinn2891 <jfinn5@ut.utm.edu>
2024-12-22 14:02:20 -0600

    button added

--- santa.js
+++ santa.js
@@ -2,6 +2,12 @@ const button = document.querySelector("#christmasbutton");
 const tree = document.querySelector("#tree");
 const page = document.querySelector("#page");
 const background = document.querySelector("#background");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => {
+    window.alert("Under Construction!");
+});
+
 button.addEventListener("click", () => {
     background.classList.toggle("change");
     tree.classList.toggle("move-right");
